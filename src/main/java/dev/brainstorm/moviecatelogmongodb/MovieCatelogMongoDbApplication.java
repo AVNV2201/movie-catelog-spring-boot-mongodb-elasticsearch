f49bc4bb3fd319ae1e3dfe551625a4a1c69121da
avnv2201 <abhinav.cs2201@gmail.com>
2021-07-30 12:30:14 +0530

    starter project

---
+++ src/main/java/dev/brainstorm/moviecatelogmongodb/MovieCatelogMongoDbApplication.java
@@ -0,0 +1,13 @@
+package dev.brainstorm.moviecatelogmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCatelogMongoDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieCatelogMongoDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/brainstorm/moviecatelogmongodb/MovieCatelogMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.brainstorm.moviecatelogmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatelogMongoDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
